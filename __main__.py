90d26b3c65ddf7b4e60c344b8406a1ac6c0a97c1
Matthew <905mjl@gmail.com>
2021-02-17 15:10:08 -0600

    api access

--- __main__.py
+++ __main__.py
@@ -1,10 +1,26 @@
 import speech_recognition as sr
+import json
+import urllib3
+
 snow_path = "C:\\Users\\mlf05\\Desktop\\answermezoom"
-jarv_path = "C:\\Users\\mlf05\\Desktop\\answermezoom\\snowboy\\resources\\models\\jarvis.umdl"
+
 r = sr.Recognizer()
 m = sr.Microphone()
-r.pause_threshold = .7
+r.pause_threshold = .6
 keyword = "hey computer"
+url = "https://rdc1nf9jza.execute-api.us-east-1.amazonaws.com/api/qna"
+api_key = "9HeBB23LR4a8mruNNf3kq17fFuR9b4JP1q5KSMCC"
+
+
+def get_answer(question: str) -> str:
+    http = urllib3.PoolManager()
+    r = http.request('GET',
+                     url,
+                     headers={'x-api-key': api_key},
+                     fields={'search_string': question})
+    result_json = json.loads(r.data)
+    return result_json['longAnswer']
+
 
 try:
     print("A moment of silence, please...")
@@ -21,9 +37,14 @@ try:
             value = r.recognize_google(audio)
             val_str = format(value)
             if keyword in val_str:
-                print("keyword detected")
-            # we need some special handling here to correctly print unicode characters to standard output
-            if str is bytes:  # this version of Python uses bytes for strings (Python 2)
+                question = val_str[val_str.find(keyword) + len(keyword) + 1:]
+                print("Question detected!")
+                print("Looking up: " + question)
+                answer = get_answer(question)
+                print("Answer: " + answer)
+                # print(get_answer(question))
+                # we need some special handling here to correctly print unicode characters to standard output
+            elif str is bytes:  # this version of Python uses bytes for strings (Python 2)
                 print(u"You said {}".format(value).encode("utf-8"))
             else:  # this version of Python uses unicode for strings (Python 3+)
                 print("You said {}".format(value))
@@ -33,3 +54,6 @@ try:
             print("Uh oh! Couldn't request results from Google Speech Recognition service; {0}".format(e))
 except KeyboardInterrupt:
     pass
+
+
+
